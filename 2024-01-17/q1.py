37a784eeea7a8b18711d261727c5db049da27698
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2024-01-17 17:37:52 +0530

    ASSIGNMENT 1

---
+++ 2024-01-17/q1.py
@@ -0,0 +1,22 @@
+#Q1 : WAP to obtain a sorted list of words with their counts & make sure that 
+#     all words are lower-cased & contain only letters from a-z.
+
+import string 
+
+text = open("/content/drive/MyDrive/Colab/24-01-17/data.txt", "r") 
+d = dict() 
+
+for line in text: 
+    line = line.strip() 
+    line = line.lower() 
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    words = line.split(" ") 
+	
+    for word in words: 
+        if word in d: 
+            d[word] = d[word] + 1
+        else: 
+            d[word] = 1
+
+for key in sorted(d.keys()): 
+	print(key, ":", d[key]) 
---
+++ 2024-01-17/q2.py
@@ -0,0 +1,12 @@
+#Q2 : WAP to pre-process the data set by removing the most common words
+#     (stopwords). For English language some examples are “the”, “and”, “if”, 
+#     “which”, and “on”.
+
+stpwrd_lst = ["the", "and", "if", "which", "on"]
+with open("/content/drive/MyDrive/Colab/24-01-17/data.txt", "r") as file:
+    txt = file.read()
+
+words = txt.split()
+fltr_txt = [word for word in words if word.lower() not in stpwrd_lst]
+fltr_txt = ' '.join(fltr_txt)
+print(fltr_txt)
