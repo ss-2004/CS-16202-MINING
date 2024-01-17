baeb34069c924701df0a7c9b0c207dfe41f8f9c1
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2024-01-17 17:33:25 +0530

    Delete 24-01-17 directory

--- 24-01-17/q2.py
+++
@@ -1,8 +0,0 @@
-stpwrd_lst = ["the", "and", "if", "which", "on"]
-with open("/content/24-01-17/data.txt", "r") as file:
-    txt = file.read()
-
-words = txt.split()
-fltr_txt = [word for word in words if word.lower() not in stpwrd_lst]
-fltr_txt = ' '.join(fltr_txt)
-print(fltr_txt)
