b9943360ffce4d52f608bfc3be9f268500130d49
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2024-01-17 11:49:29 +0530

    ASSIGNMENT 1

--- 24-01-17/q1.py
+++ 24-01-17/q1.py
@@ -1,1 +1,19 @@
+import string
 
+text = open("/content/24-01-17/data.txt", "r") 
+d = dict()
+
+for line in text: 
+    line = line.strip() 
+    line = line.lower()
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    words = line.split(" ")
+					
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+
+for key in list(d.keys()): 
+    print(key, "\t\t : ", d[key]) 
