3a9276875d8dd1f64425a1aedfc109fef026a5a4
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2024-04-30 21:27:41 +0530

    Assignment 8, 9, 10

--- 2024-04-24/assgn9/q3.py
+++ 2024-04-24/assgn9/q3.py
@@ -1,3 +1,7 @@
+# Implementation of classification using j-48 algorithm in python on ‘weather.nominal.arff’
+# (storm.cis.fordham.edu/~gweiss/data-mining/weka-data/weather.nominal.arff) dataset & verify the result
+# with Objective 1.
+
 import pandas as pd
 from weka.classifiers import Classifier
 from weka.core.converters import Loader
@@ -24,3 +28,24 @@ print("Kappa:", evaluation.kappa())
 # Extract if-then rules
 print("If-then rules:")
 print(cls)
+
+# from sklearn import datasets
+# from sklearn.model_selection import train_test_split 
+# from sklearn.preprocessing import OneHotEncoder 
+# from sklearn import tree
+# import pandas as pd
+# # Load dataset
+# filename = '/Users/ShresthS/Desktop/CSE/ASSGN/SEM6/MINE/2024-04-24/assgn8/Buy_Computer.csv'
+# df = pd.read_csv(filename)
+# # Preprocess dataset
+# enc = OneHotEncoder() 
+# enc.fit(weather.data)
+# X = enc.transform(weather.data)
+# # Split dataset into training set and test set
+# X_train, X_test, y_train, y_test = train_test_split(X, weather.target, test_size=0.3)
+# # Create Decision Tree classifer object 
+# clf = tree.DecisionTreeClassifier()
+# # Train Decision Tree Classifer 
+# clf = clf.fit(X_train,y_train)
+# # Predict the response for test dataset 
+# y_pred = clf.predict(X_test)
